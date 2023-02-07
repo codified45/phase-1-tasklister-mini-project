eaa62172354a380308c21d93bd8a0d7facc2d03d
Jack Lach <jack.lach4@gmail.com>
2023-02-06 21:06:50 -0500

    deliverables completed

--- src/index.js
+++ src/index.js
@@ -3,24 +3,58 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-
+  document.querySelector('#create-task-form').addEventListener('submit', e => {
+    e.preventDefault();
+    buildToDo(e.target['new-task-description'].value);
+  });
 });
+
+const buildToDo = (toDoItem) => {
+  console.log(toDoItem);
+  let p = document.createElement('p');
+  p.textContent = `${toDoItem}      `;
+  document.querySelector('#create-task-form').reset(); 
+  document.querySelector('#tasks').appendChild(p);
+  let dltBtn = document.createElement('button');
+  dltBtn.addEventListener('click', handleDelete)
+  dltBtn.textContent = "x";
+  p.appendChild(dltBtn);
+  console.log(dltBtn.innerHTML);
+  console.log(dltBtn)
+} 
+
+const handleDelete = e => {
+  e.target.parentNode.remove();
+}
+
+
 
 // Deliverables:
 
 // suppress default action of form (auto refresh of page) == Event.preventDefault()
+// done:
+  // e.preventDefault();
 
 // As a user, I should be able to type a task into the input field.
+// done:
+  // <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
 
 // As a user, I should be able to click some form of a submit button.
+// done: 
+  // <input type="submit" value="Create New Task">
+  // addEventListener('submit', e => {...} )
 
 // As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+// done:
+  // document.querySelector('#tasks').appendChild(p);
 
 
 
-// Stretch Deliverables:
+// ** Stretch Deliverables:
 
 // A delete function that will remove tasks from your list
+// done:
+  // dltBtn.addEventListener('click', handleDelete)
 
 // A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
 

05bf0e5f3acf8a360cb514921f4f0f0213ab72f2
Jack Lach <jack.lach4@gmail.com>
2023-02-08 00:12:45 -0500

    working on prioritize button functionality through flex order property

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('#create-task-form').addEventListener('submit', e => {
-    e.preventDefault();
+    e.preventDefault();   // in this case, only needed for the form because the submit would auto-refresh the page due to how forms used to work. 
     buildToDo(e.target['new-task-description'].value);
   });
+
+  document.getElementById('prioritize_button').addEventListener('click', prioritize);
 });
 
+// const toDoArray = [];  // bad idea since it doesn't update with the DOM. Grabbing an HTML collection with getElementsByClassName() does.
+
 const buildToDo = (toDoItem) => {
-  console.log(toDoItem);
   let p = document.createElement('p');
   p.textContent = `${toDoItem}      `;
   let taskColor = document.querySelector('#new_task_priority').value;
   p.style.color = taskColor;
+  p.classList.add(taskColor, "userAddedItem"); // probably not needed
+  console.log(p);
 
   document.querySelector('#create-task-form').reset(); 
   
@@ -23,17 +28,26 @@ const buildToDo = (toDoItem) => {
   dltBtn.addEventListener('click', handleDelete);
   dltBtn.textContent = "x";
   p.appendChild(dltBtn);
-  console.log(dltBtn.innerHTML);
-  console.log(dltBtn)
 } 
 
 const handleDelete = e => {
   e.target.parentNode.remove();
 }
 
-const getPriority = () => {
 
-}
+// look up how to redraw all the elements in container so that the order property of the flex is respected.  Ohhhh mayb because it is a list instead of a div!!!!! Or might have to store the elements in some sort of array and refresh the page? Hm not sure. Lookup "reordering elements in DOM"
+const prioritize = () => {
+  console.log('clicked');
+  console.log(document.getElementsByClassName('userAddedItem'));
+  let allUserTasks = document.getElementsByClassName('userAddedItem')
+  for (const task of allUserTasks) {
+    if (task.style.color === "yellow") {task.style.order = 1};
+    if (task.style.color === "green") {task.style.order = 2};
+  };
+  const taskList = document.getElementById('tasks');
+  taskList.replaceChildren(...allUserTasks);
+  console.log(allUserTasks);
+};
 
 
 // Deliverables:
@@ -65,6 +79,9 @@ const getPriority = () => {
   // const handleDelete = e => {e.target.parentNode.remove()}
 
 // A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+//done:
+  // let taskColor = document.querySelector('#new_task_priority').value;
+  // p.style.color = taskColor;
 
 // As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
 // Bubble?

2012fe0ed8bcd6803f02f92602e187ff0862abc1
Jack Lach <jack.lach4@gmail.com>
2023-02-09 17:44:51 -0500

    stretch deliverable: adding additional input field == completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('#create-task-form').addEventListener('submit', e => {
     e.preventDefault();   // in this case, only needed for the form because the submit would auto-refresh the page due to how forms used to work. 
-    buildToDo(e.target['new-task-description'].value);
+    buildToDo(e.target['new-task-description'].value, e.target['estimated_time'].value);
   });
 
   document.getElementById('prioritize_button').addEventListener('click', prioritize);
@@ -13,13 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // const toDoArray = [];  // bad idea since it doesn't update with the DOM. Grabbing an HTML collection with getElementsByClassName() does.
 
-const buildToDo = (toDoItem) => {
+const buildToDo = (toDoItem, toDoTime) => {
+  if (toDoTime > 180) {
+    alert('Breaking tasks into smaller elements that take less time has shown to help make tasks less daunting and promote completion. Please try dividing the bigger task into smaller elements.');
+    return;}
   let p = document.createElement('p');
-  p.textContent = `${toDoItem}      `;
+  p.textContent = `${toDoItem}, ${toDoTime} minutes     `;
   let taskColor = document.querySelector('#new_task_priority').value;
   p.style.color = taskColor;
-  p.classList.add(taskColor, "userAddedItem"); // probably not needed
-  console.log(p);
+  p.classList.add(taskColor, "userAddedItem");
 
   document.querySelector('#create-task-form').reset(); 
   
@@ -28,13 +30,13 @@ const buildToDo = (toDoItem) => {
   dltBtn.addEventListener('click', handleDelete);
   dltBtn.textContent = "x";
   p.appendChild(dltBtn);
-} 
+}
 
 const handleDelete = e => {
   e.target.parentNode.remove();
 }
 
-// Might have to  turn the HTML collection into an array first so that we could access array methods like .sort(), but I thought flex order was a way around this...
+
 const prioritize = () => {
   console.log('clicked');
   console.log(document.getElementsByClassName('userAddedItem'));
@@ -64,24 +66,29 @@ const prioritize = () => {
 };
 
 
+
 // Deliverables:
 
 // suppress default action of form (auto refresh of page) == Event.preventDefault()
 // done:
   // e.preventDefault();
 
+
 // As a user, I should be able to type a task into the input field.
 // done:
   // <input type="text" id="new-task-description" name="new-task-description" placeholder="description">
+
 
 // As a user, I should be able to click some form of a submit button.
 // done: 
   // <input type="submit" value="Create New Task">
   // addEventListener('submit', e => {...} )
 
+
 // As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
 // done:
   // document.querySelector('#tasks').appendChild(p);
+
 
 
 // ** Stretch Deliverables:
@@ -91,10 +98,12 @@ const prioritize = () => {
   // dltBtn.addEventListener('click', handleDelete)
   // const handleDelete = e => {e.target.parentNode.remove()}
 
+
 // A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
 //done:
   // let taskColor = document.querySelector('#new_task_priority').value;
   // p.style.color = taskColor;
+
 
 // As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
 // done:
@@ -108,12 +117,23 @@ const prioritize = () => {
   //   if (task.style.color === "green") {task.style.order = 3};
   // }
 
+
 // An additional input field (e.g. user, duration, date due)
-// add <input>
-// do time estimated to complete. If the time estimated is longer than 3 hours, then tell the user to break it up into smaller part. have a dialog (alert box) pop up that says "breaking tasks into smaller elements has shown to help make tasks less daunting and promote completion"
+// done:
+
+  // HTML:
+  // <label for="estimated_time">Estimated time required to complete:</label>
+  // <input type="text" id="estimated_time" name="estimated_time" placeholder="time in minutes">
+
+  // Javascript:
+  // if (toDoTime > 180) {
+  //     alert('Breaking tasks into smaller elements that take less time has shown to help make tasks less daunting and promote completion. Please try dividing the bigger task into smaller elements.');
+  //     return;}
+
 
 // Ability to edit tasks
 // somehow reassign textContent according to what user inputs (might have to create an input field when the user clicks edit button)
+
 
 // Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
 // boost priority button. Wow that's actually a great idea.  just change p.style.color to one level above. The order value will be reassigned the next time prioritize! is clicked.  
